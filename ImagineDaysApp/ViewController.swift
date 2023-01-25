f586fe1949a492e58849a35cfcf2a2630ad1e871
Harry Vologodskii <49877717+GarikVologodskiy@users.noreply.github.com>
2023-01-25 17:21:08 +0500

    first version

--- ImagineDaysApp/ViewController.swift
+++ ImagineDaysApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+//    @IBOutlet weak var resultButton: UIButton!
+    private var numberOfDays = ""
+    
+//    let enUS = Locale(identifier: "en-US")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //corner rounding
+//        resultButton.layer.cornerRadius = 12
     }
 
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+            
+    }
 
+    @IBAction func resultButtonTapped() {
+        infoLabel.text = "You've been enjoying life for \(numberOfDays)"
+    }
 }
 
